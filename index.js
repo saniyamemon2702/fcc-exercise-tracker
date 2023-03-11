bcfd734ab734726d62e9d7303d371113030b2d99
saniyamemon2702 <saniyamemon7862@gmail.com>
2023-03-11 13:48:49 -0500

    C:/Program Files/Git/api/users GET request completed!

--- index.js
+++ index.js
@@ -3,6 +3,46 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+// Body Parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// Mongoose
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://admin:admin@saniyacluster.xrzit.mongodb.net/excerciseDB?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+
+// Schema
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  username: {type: String, required: true},
+  excercises: [{description: String, duration: Number, date: Date}]
+})
+
+// Model
+const User = mongoose.model('User', userSchema)
+
+// get all the users
+app.get("/api/users", (req, res) => {
+  User.find().then((docs) => {
+    const users = docs.map((doc) => {
+      return { _id: doc._id, username: doc.username, __v: doc.__v }
+    })
+    res.send(users)
+  }).catch(err => console.log(err))
+})
+
+// // Post new user
+// app.post('/api/users', (req, res) => {
+//   const newUser = new User({username: req.body.username})
+//   newUser.save().then((doc) => {
+//     res.json({_id: doc._id, username: doc.username})
+//   }).catch((err) => {
+//     console.error(err)
+//   })
+// })
+
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
