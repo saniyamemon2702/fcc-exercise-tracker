ed2bd15c28705a71d2206110924b76659c501035
saniyamemon2702 <saniyamemon7862@gmail.com>
2023-03-14 20:48:30 -0400

    /api/users/:id/exercises POST method

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+// Cors
+
+app.use(cors())
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+
 // Mongoose
 const mongoose = require('mongoose')
 mongoose.connect("mongodb+srv://admin:admin@saniyacluster.xrzit.mongodb.net/excerciseDB?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
@@ -15,8 +24,8 @@ mongoose.connect("mongodb+srv://admin:admin@saniyacluster.xrzit.mongodb.net/exce
 // Schema
 const Schema = mongoose.Schema
 const userSchema = new Schema({
-  username: {type: String, required: true},
-  excercises: [{description: String, duration: Number, date: Date}]
+  username: {type: String},
+  excercises: [{description: String, duration: Number, date: String}]
 })
 
 // Model
@@ -38,15 +47,37 @@ app.post("/api/users", (req, res) => {
   let newUser = new User({ username: req.body.username });
   newUser.save().then((doc) => {
     res.json({ username: doc.username, _id: doc.id });
-  }).catch((err) => console.log(err));
+  }).catch((err) => console.log(err.message,err.code));
 })
 
+// /api/users/:id/exercise post request
+app.post("/api/users/:id/exercises", (req, res) => {
+  
+  // res.json(req.body);
+  const userId = req.params.id;
+  // console.log("body ID",req.body._id);
+  const newExcercise = {
+    description: req.body.description,
+    duration: req.body.duration,
+    date: req.body.date? new Date(req.body.date).toDateString(): new Date().toDateString()
+  };
+  // console.log(newExcercise);
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+  // find the user by id and add the excercise
+  User.findById(userId).then((doc) => {
+    if(!doc) return res.send("User not found")
+    else {
+      console.log("User found");
+    // console.log("username",doc.username);
+    doc.excercises.push(newExcercise);
+    doc.save().then((doc) => {
+      console.log("Excercise added!");
+      res.json({_id:doc._id,username:doc.username, date:doc.excercises[doc.excercises.length-1].date, duration:doc.excercises[doc.excercises.length-1].duration, description:doc.excercises[doc.excercises.length-1].description});
+      // res.json({_id:doc._id,username:doc.username, date:new Date(newExcercise.date).toDateString(), duration:newExcercise.duration, description:newExcercise.description});
+    }).catch(err => console.log(err.message))
+  }
+  }).catch(err => console.log(err.message))
+})
 
 
 
