fb39fd3416d55836f99840d8f60f7d99788656d4
saniyamemon2702 <saniyamemon7862@gmail.com>
2023-03-19 18:10:14 -0400

    get request to api/users/:id/logs?[from][&to][&limit] successfully added!

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ mongoose.connect("mongodb+srv://admin:admin@saniyacluster.xrzit.mongodb.net/exce
 // Schema
 const Schema = mongoose.Schema
 const userSchema = new Schema({
-  username: {type: String},
-  excercises: [{description: String, duration: Number, date: String}]
+  username: { type: String },
+  excercises: [{ description: String, duration: Number, date: String }]
 })
 
 // Model
@@ -47,42 +47,90 @@ app.post("/api/users", (req, res) => {
   let newUser = new User({ username: req.body.username });
   newUser.save().then((doc) => {
     res.json({ username: doc.username, _id: doc.id });
-  }).catch((err) => console.log(err.message,err.code));
+  }).catch((err) => console.log(err.message, err.code));
 })
 
 // /api/users/:id/exercise post request
 app.post("/api/users/:id/exercises", (req, res) => {
-  
+
   // res.json(req.body);
   const userId = req.params.id;
   // console.log("body ID",req.body._id);
   const newExcercise = {
     description: req.body.description,
     duration: req.body.duration,
-    date: req.body.date? new Date(req.body.date).toDateString(): new Date().toDateString()
+    date: req.body.date ? new Date(req.body.date).toDateString() : new Date().toDateString()
   };
   // console.log(newExcercise);
 
   // find the user by id and add the excercise
   User.findById(userId).then((doc) => {
-    if(!doc) return res.send("User not found")
+    if (!doc) return res.send("User not found")
     else {
       console.log("User found");
-    // console.log("username",doc.username);
-    doc.excercises.push(newExcercise);
-    doc.save().then((doc) => {
-      console.log("Excercise added!");
-      res.json({_id:doc._id,username:doc.username, date:doc.excercises[doc.excercises.length-1].date, duration:doc.excercises[doc.excercises.length-1].duration, description:doc.excercises[doc.excercises.length-1].description});
-      // res.json({_id:doc._id,username:doc.username, date:new Date(newExcercise.date).toDateString(), duration:newExcercise.duration, description:newExcercise.description});
-    }).catch(err => console.log(err.message))
-  }
+      // console.log("username",doc.username);
+      doc.excercises.push(newExcercise);
+      doc.save().then((doc) => {
+        console.log("Excercise added!");
+        res.json({ _id: doc._id, username: doc.username, date: doc.excercises[doc.excercises.length - 1].date, duration: doc.excercises[doc.excercises.length - 1].duration, description: doc.excercises[doc.excercises.length - 1].description });
+        // res.json({_id:doc._id,username:doc.username, date:new Date(newExcercise.date).toDateString(), duration:newExcercise.duration, description:newExcercise.description});
+      }).catch(err => console.log(err.message))
+    }
   }).catch(err => console.log(err.message))
 })
 
+// /api/users/:id/log get request
+app.get("/api/users/:id/logs", (req, res) => {
+  const userId = req.params.id;
+  console.log(req.query.from, req.query.to, req.query.limit);
+  const fromDate = req.query.from;
+  const toDate = req.query.to;
+  const limit = req.query.limit;
+  User.findById(userId).then((doc) => {
+    if (!doc) return res.send("User not found")
+    else {
+      console.log("User found");
+      if (fromDate || toDate || limit) {
+        console.log("from", fromDate, "to", toDate, "limit", limit);
+        const log = [];
+        doc.excercises.forEach((excercise) => {
 
+          if (fromDate && toDate) {
+            if (new Date(excercise.date).getTime() >= new Date(fromDate).getTime() && new Date(excercise.date).getTime() <= new Date(toDate).getTime()) {
+              log.push(excercise);
+            }
+          }
+          else if (fromDate) {
+            if (new Date(excercise.date).getTime() >= new Date(fromDate).getTime()) {
+              log.push(excercise);
+            }
+          }
+          else if (toDate) {
+            if (new Date(excercise.date).getTime() <= new Date(toDate).getTime()) {
+              log.push(excercise);
+            }
+          }
+          else {
+            log.push(excercise);
+          }
+        })
+        if (limit) {
+         log.splice(limit);
+        }
+        res.json({ _id: doc._id, username: doc.username, count: log.length, log: log });
 
+      }
+      else {
+        console.log("No date range");
+        res.json({ _id: doc._id, username: doc.username, count: doc.excercises.length, log: doc.excercises });
+      }
+      // res.json({ _id: doc._id, username: doc.username, count: doc.excercises.length, log: doc.excercises});
+    }
+  })
+})
 
-
+// listen for requests :)
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
